d8488b429f924c837bed9a16dacb4989844c35d1
Deep Varkute <deepvarkute04@gmail.com>
2025-10-30 18:46:46 +0530

    Update func.js

--- func.js
+++ func.js
@@ -1,14 +1,51 @@
-function check(num){
-if (num%2!=0){
-console.log("Number is odd")
-}
-else if(num==0){
-console.log("Number is 0")
-}
-else{
-console.log("Number is even")
-}
-}
-check(0)
-check(2)
-check(3)
+5.Write a program to implement ES6 functions.
+
+
+code1:
+leap.js
+
+const isLeapYear = (year) => {
+  if ((year % 4 === 0 && year % 100 !== 0) || (year % 400 === 0)) {
+    console.log(`${year} is a leap year`);
+  } else {
+    console.log(`${year} is not a leap year`);
+  }
+};
+
+isLeapYear(2024); 
+isLeapYear(2023);
+
+----------------------------------
+node leap.js
+
+
+
+
+code2:
+addition.js
+const sumSeries = (...numbers) => {   
+  let sum = 0;
+  for (let num of numbers) {          
+    sum += num;                       
+  }
+  return sum;                         
+};
+
+console.log(sumSeries(1, 2, 3, 4, 5));  
+console.log(sumSeries(10, 20, 30));      
+------------------------------------
+node addition.js
+
+
+
+
+code3:
+anonymous.js
+
+const greet = function() {
+  console.log("Hello, welcome!");
+};
+
+greet();
+--------------------------------------
+node anonymous.js
